355f213a2bbd125a390fe0222f740904f5abfcdb
Viki Harrod <victoria.harrod@gmail.com>
2014-09-29 13:29:46 -0600

    removed unnecessary code pertaining to legislators, erb and form letters

--- lib/data_cleaner.rb
+++ lib/data_cleaner.rb
@@ -1,8 +1,3 @@
-require 'sunlight/congress'
-require 'erb'
-
-Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
@@ -42,32 +37,10 @@ def incorrect_prefix?(phone_number)
   phone_number.length == 11 && phone_number[0] != "1"
 end
 
-def legislators_by_zipcode(zipcode)
-  Sunlight::Congress::Legislator.by_zipcode(zipcode)
-end
-
-def save_thank_you_letters(id, form_letter)
-  Dir.mkdir("output") unless Dir.exists? "output"
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
-end
-
-template_letter = File.read "form_letter.erb"
-erb_template = ERB.new template_letter
-
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   phone_number = clean_phone_number(row[:homephone])
   zipcode = clean_zipcode(row[:zipcode])
   puts "#{name} #{zipcode} #{phone_number}"
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letters(id, form_letter)
 end
