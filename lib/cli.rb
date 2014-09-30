9ab0a345c20fdbfbe1f178fabf42c3ff3cb97836
Viki Harrod <victoria.harrod@gmail.com>
2014-09-29 20:27:23 -0600

    refactored cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,36 +11,51 @@ class CLI
 
   def start
     printer.welcome
+    process_initial_commands
+  end
+
+  def process_initial_commands
     until quit?
     @command = gets.strip
-    case
-    when command == "load"
-      repository
-    when command == "help"
-      printer.help
-    when command == "queue count"
-      printer.queue_count(queue.count)
-    when command == "queue print"
-      queue.print_queue
-    when command == "queue clear"
-      queue.clear
-    when command =~ /(find)/
-      queue.clear
-      command, attribute, criteria = @command.split(" ")
       case
-      when attribute == "first_name"
-        results = repository.find_by_first_name(criteria)
-        results.each do |result|
-          queue.add_to_queue(result.name)
-        end
-      when attribute == "last_name"
-        results = repository.find_by_last_name(criteria)
-        results.each do |result|
-          puts result.name
-        end
+      when command == "load"
+        repository
+      when command == "help"
+        printer.help
+      when command == "queue count"
+        printer.queue_count(queue.count)
+      when command == "queue print"
+        queue.print_queue
+      when command == "queue clear"
+        queue.clear
+      when command =~ /(find)/
+        find_by_attribute_and_criteria
       end
-    when attribute == ""
     end
+  end
+
+  def find_by_attribute_and_criteria
+    queue.clear
+    command, attribute, criteria = @command.split(" ")
+    case
+    when attribute == "first_name"
+      lookup_first_name(criteria)
+    when attribute == "last_name"
+      lookup_last_name(criteria)
+    end
+  end
+
+  def lookup_first_name(criteria)
+    results = repository.find_by_first_name(criteria)
+    results.each do |result|
+      queue.add_to_queue(result.name)
+    end
+  end
+
+  def lookup_last_name(criteria)
+    results = repository.find_by_last_name(criteria)
+    results.each do |result|
+      queue.add_to_queue(result.name)
     end
   end
 
